09da0ed4b042800e58d8fa3c38efadaff98d2aa1
Udara San <udarassanjeewa@gmail.com>
2025-06-09 14:36:37 +0530

    how to change 3rd party beans ids

--- 01_Spring_Core/src/main/java/org/example/AppInitializer.java
+++ 01_Spring_Core/src/main/java/org/example/AppInitializer.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.bean.SpringBean;
-import org.example.bean.TestBean1;
-import org.example.bean.TestBean2;
-import org.example.bean.TestBean3;
+import org.example.bean.MyConnection;
 import org.example.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,14 +10,17 @@ public class AppInitializer {
         context.register(AppConfig.class);
         context.refresh();
 
-        TestBean1 testBean1=context.getBean(TestBean1.class);
+        /*TestBean1 testBean1=context.getBean(TestBean1.class);
         System.out.println(testBean1);
 
         //request bean from bean ID(Class Name (first letter ->simple letter)
         TestBean1 byBeanID= (TestBean1) context.getBean("testBean1");
         System.out.println(byBeanID);
         TestBean2 testBean2=context.getBean("udara",TestBean2.class);
-        System.out.println(testBean2);
+        System.out.println(testBean2);*/
+        MyConnection connection= (MyConnection)
+                context.getBean("san");
+        System.out.println(connection);
 
         context.registerShutdownHook();
 
--- 01_Spring_Core/src/main/java/org/example/config/AppConfig.java
+++ 01_Spring_Core/src/main/java/org/example/config/AppConfig.java
@@ -1,15 +1,17 @@
 package org.example.config;
 
 import org.example.bean.MyConnection;
+import org.example.bean.TestBean1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.bean")
+//@ComponentScan(basePackageClasses = TestBean1.class)
 public class AppConfig {
-    @Bean
-    MyConnection myConnection(){
+    @Bean("san")
+    MyConnection getConnection(){
         return new MyConnection();
     }
 }
