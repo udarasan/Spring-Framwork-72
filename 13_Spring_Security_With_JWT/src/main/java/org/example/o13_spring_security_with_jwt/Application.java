250edcbb1cf29b36dabaa41ad30ce27579c3b257
Udara San <udarassanjeewa@gmail.com>
2025-07-21 10:38:41 +0530

    spring security

---
+++ 13_Spring_Security_With_JWT/src/main/java/org/example/o13_spring_security_with_jwt/Application.java
@@ -0,0 +1,13 @@
+package org.example.o13_spring_security_with_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ 13_Spring_Security_With_JWT/src/test/java/org/example/o13_spring_security_with_jwt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.o13_spring_security_with_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
