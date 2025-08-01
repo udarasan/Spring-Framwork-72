11817de17eb72b58e091f41cb14b838da990b7d4
Udara San <udarassanjeewa@gmail.com>
2025-08-01 14:10:27 +0530

    testing

---
+++ 14_Spring_Testing/src/test/java/org/example/o14_spring_testing/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.o14_spring_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
